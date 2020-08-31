c88a5d8a8f49f7a527488728f03754aae91edd96
Xiao Zhang <xz9rc@umsystem.edu>
2020-08-31 17:04:15 -0500

    Displaying recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,9 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     // tslint:disable-next-line: max-line-length
-    new Recipe('A test recipe', 'test', 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+    new Recipe('A test recipe', 'test', 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test recipe', 'test', 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
   ];
 
   constructor() { }
