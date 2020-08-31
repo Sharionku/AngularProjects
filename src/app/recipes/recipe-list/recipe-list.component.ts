50548af5c6e91462fe3940a190b2b04a2bb90367
Xiao Zhang <xz9rc@umsystem.edu>
2020-08-31 15:41:04 -0500

    prj-project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test recipe', 'test', 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
